2b2e8bce26b995493f52391cf51ba7e85c1db6ec
Michal Cernik <michalcernik@gmail.com>
2023-07-01 12:02:14 +0200

    Skeleton functionality, tests.

---
+++ SimpleReportModel.Tests/ReportDataProviderTest.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using System.Text.Json;
+
+namespace SimpleReportModel.Tests;
+
+public class ReportDataProviderTest
+{
+  private readonly Mock<IProvideJsonResult> jsonResultProviderMock;
+  private readonly ReportDataProvider testedInstance;
+
+  public ReportDataProviderTest()
+  {
+    jsonResultProviderMock = new Mock<IProvideJsonResult>();
+
+    testedInstance = new ReportDataProvider(jsonResultProviderMock.Object);
+  }
+
+  [Fact]
+  public void GetReturnsEmptyWhenNoRows()
+  {
+    jsonResultProviderMock.Setup(m => m.GetQueryResult(It.IsAny<string>(), It.IsAny<SqlConnection>()))
+      .Returns(() => "[]");
+
+    var returnedData = testedInstance.Get<EmptyClass>(string.Empty, null);
+
+    returnedData.Should().NotBeNull();
+    returnedData.Should().BeEmpty();
+  }
+
+  [Fact]
+  public void GetReturnsExpectedNumberOfEntities()
+  {
+    jsonResultProviderMock.Setup(m => m.GetQueryResult(It.IsAny<string>(), It.IsAny<SqlConnection>()))
+      .Returns(() => "[{},{}]");
+
+    var returnedData = testedInstance.Get<EmptyClass>(string.Empty, null);
+
+    returnedData.Should().NotBeNullOrEmpty();
+    returnedData.Count.Should().Be(2);
+  }
+
+  [Fact]
+  public void GetThrowsOnRootEntity()
+  {
+    jsonResultProviderMock.Setup(m => m.GetQueryResult(It.IsAny<string>(), It.IsAny<SqlConnection>()))
+      .Returns(() => "{}");
+
+    var action = () => testedInstance.Get<EmptyClass>(string.Empty, null);
+
+    action.Should().Throw<JsonException>();
+  }
+
+  public class EmptyClass
+  { }
+}
---
+++ SimpleReportModel/ReportDataProvider.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Text.Json;
+
+namespace SimpleReportModel;
+
+public class ReportDataProvider : IProvideReportData
+{
+  private readonly IProvideJsonResult jsonProvider;
+
+  public ReportDataProvider(IProvideJsonResult jsonProvider)
+  {
+    this.jsonProvider = jsonProvider;
+  }
+  
+  public IReadOnlyCollection<T> Get<T>(string queryForJson, SqlConnection connection, Action<JsonSerializerOptions> setupOptions = null)
+    where T : class
+  {
+    var json = jsonProvider.GetQueryResult(queryForJson, connection);
+    
+    var serializerOptions = new JsonSerializerOptions();
+    if (setupOptions != null)
+      setupOptions(serializerOptions);
+    
+    return JsonSerializer.Deserialize<T[]>(json, serializerOptions);
+  }
+}
